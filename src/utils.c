8c6e03a7c260f57485c8915edc550f779099d918
Evan Maddock <maddock.evan@vivaldi.net>
2021-08-07 16:50:59 -0400

    Remove unused functions
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/utils.c
+++ src/utils.c
@@ -3,28 +3,3 @@
 gboolean trash_utils_is_string_valid(gchar *string) {
     return (string != NULL && g_strcmp0(string, "") != 0);
 }
-
-gdouble trash_utils_logn(gdouble n, gdouble base) {
-    return log(n) / log(base);
-}
-
-gchar *trash_utils_substring(gchar *source, gint offset, size_t length) {
-    size_t source_len = strlen(source);
-
-    // Make sure we aren't trying to copy past the length of the source string
-    if ((offset + length > source_len) && length != source_len) {
-        return NULL;
-    }
-
-    if (length == source_len) {
-        // Set the length to the number of chars being copied
-        length = length - offset;
-    }
-
-    gchar *dest = malloc(sizeof(gchar) * length + 1);
-    g_return_val_if_fail(dest != NULL, NULL);
-
-    strncpy(dest, source + offset, length);
-    dest[length] = '\0';
-    return dest;
-}
--- src/utils.h
+++ src/utils.h
@@ -11,16 +11,3 @@
  * Checks if a string is not `NULL` and not empty.
  */
 gboolean trash_utils_is_string_valid(gchar *string);
-
-/**
- * Calculate the logarithm of a number for a given base.
- */
-gdouble trash_utils_logn(gdouble n, gdouble base);
-
-/**
- * Returns a new string consisting of the substring of the full string
- * starting at `offset` and going until `length`.
- *
- * The returned string should be freed with `g_free()`.
- */
-gchar *trash_utils_substring(gchar *source, gint offset, size_t length);
